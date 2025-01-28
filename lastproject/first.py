c900cd7ee4f290a837637edbec01929646fabc27
Ram <rtaysf@gmail.com>
2025-01-28 11:55:43 +0530

    updated the details

---
+++ lastproject/first.py
@@ -0,0 +1,10 @@
+def ram():
+   for i in range(100):
+       if i<51:
+           if i%2==0:
+              print(f"this is an even number {i}")
+       else:
+          if i%2!=0:
+              print(f"This is an odd number from 51 {i}")
+ram()           
+
